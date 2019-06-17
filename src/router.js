fb886af64c081975d611b826ab2e842d821d534e
licy <licy@briup.com>
2019-06-17 11:36:14 +0800

    '初始化提交'

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router, Route, Switch } from 'dva/router';
+import IndexPage from './routes/IndexPage';
+import ProductPage from './routes/ProductPage'
+
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={IndexPage} />
+        <Route path="/product" exact component={ProductPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default RouterConfig;
---
+++ src/routes/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  render(){
+    <div>
+      <div>
+        {/* 动态路由 */}
+      </div>
+      <div>
+        {/* 导航 */}
+        
+      </div>
+    </div>
+  }
+}
+export default App;
